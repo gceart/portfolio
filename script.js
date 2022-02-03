04caff3f48a1de1ccc9a3f3652c19e028b824ebf
GaelCeart <gaelceart@gmail.com>
2022-02-03 18:06:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const changingMsgEl = document.getElementById("changing-msg");
+
+const phrases = ["xd", "lol", "lmao"]
+
+/* 
+    select phrase
+    pass length to widt
+
+*/
+
+const changeMsg = (msg) => {
+    changingMsgEl.style. kkkk
+}
