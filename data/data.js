52df544e69d19ca5105c5127bf4d5dbbb237691a
Gaston <devceart@gmail.com>
2024-05-02 00:14:18 -0300

    INIT preact

---
+++ data/data.js
@@ -0,0 +1,56 @@
+export const info = {
+  about: "I've been a passionate of technology since I was a little kid. <br><br>Currently I'm a student of Bachelor's Degree of Computer Systems.<br><br>I would like improve my abilities and become a full-stack developer.<br><br>In order to keep learning, I'm constantly following online courses on video."
+}
+
+export const phrases = ['think', 'resolve problems', 'learn']
+
+export const projects = [
+  {
+    name: 'Color Flipper',
+    description: 'Press the button to change the background colour to a new randomly generated colour. Also you can copy the hex value to clipboard!',
+    image: './img/projects/colorFlipper.png',
+    class: 'color-flipper',
+    link: 'https://gaelceart.github.io/color-flipper/',
+    repo: 'https://www.github.com/gaelceart/color-flipper/'
+  },
+  {
+    name: 'Posts Section',
+    description: 'Posts section with lazy load using Intersection Observer and fetch to load the data file. Comments will attach to the post',
+    image: './img/projects/lazyLoad.png',
+    class: 'lazy-load',
+    link: 'https://gaelceart.github.io/lazy-load/',
+    repo: 'https://www.github.com/gaelceart/lazy-load/'
+  },
+  {
+    name: 'w-UNGS-dle',
+    description: 'Wordle-like game. Try to guess the secret word in six tries by hints of your previous answers',
+    image: './img/projects/wungsdle.png',
+    class: 'wungsdle',
+    link: '',
+    repo: 'https://github.com/Leandr0Fr/p3-tp1-wordle'
+  },
+  {
+    name: 'Clustering',
+    description: "Cluster generator based on given vertices. Using Kruskal's algorithm, a MST is created from a complete graph to subsequently generate clusters",
+    image: './img/projects/clustering.png',
+    class: 'clustering',
+    link: '',
+    repo: 'https://github.com/locomaldelcoco/Clustering'
+  },
+  {
+    name: 'Álbum mundial 2022',
+    description: 'Simulator with perspective on sales of the FIFA 2022 World Cup figurines album',
+    image: './img/projects/albumMundial.png',
+    class: 'album-mundial',
+    link: '',
+    repo: 'https://github.com/gaelceart/album-mundial'
+  },
+  {
+    name: 'Supermielle',
+    description: "Credit Card System of a bank we called 'Supermielle'. It was done with PostgreSQL and a little demo of BoltDB",
+    image: './img/projects/supermielle.png',
+    class: 'supermielle',
+    link: '',
+    repo: 'https://gitlab.com/gastonv/martinelli-soria-villalba-tp'
+  }
+]
---
+++ src/app.jsx
@@ -0,0 +1,7 @@
+import { useState } from 'preact/hooks'
+
+export function App () {
+  return (
+    <h1 className='text-blue-50'>hello world</h1>
+  )
+}
